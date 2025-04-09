3538a65b55ffd51c0f483b9e5709a9bb90cf8e89
G. David Karssen <gdk@mailbox.org>
2025-04-09 06:50:50 +0200

    remove obsolete lines

--- internal/pkg/capture/writer.go
+++ internal/pkg/capture/writer.go
@@ -1,7 +1,6 @@
 package capture
 
 import (
-	"fmt"
 	"os"
 	"time"
 
@@ -20,10 +19,6 @@ func CloseWriters() {
 func WriteSIP(callID string, packet gopacket.Packet) {
 	mu.Lock()
 	defer mu.Unlock()
-
-	for i, layer := range packet.Layers() {
-		fmt.Printf("Layer %d: %s\n", i, layer.LayerType())
-	}
 
 	if call, ok := callMap[callID]; ok && call.SIPWriter != nil {
 		call.SIPWriter.WritePacket(packet.Metadata().CaptureInfo, packet.Data())
